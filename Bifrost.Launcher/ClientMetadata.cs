6a1bed45b2439acbd443f927db31bf1b48af0a6b
Crypto137 <Crypto137@users.noreply.github.com>
2024-07-18 07:40:27 +0300

    Move client metadata to an external file

---
+++ Bifrost.Launcher/ClientMetadata.cs
@@ -0,0 +1,12 @@
+﻿namespace Bifrost.Launcher
+{
+    public class ClientMetadata
+    {
+        public string Version { get; }
+
+        public ClientMetadata(string version)
+        {
+            Version = version;
+        }
+    }
+}
