c2fde021fb70ec0a567b1c75c0a21b57add20415
Crypto137 <Crypto137@users.noreply.github.com>
2025-11-25 15:52:24 +0300

    Avalonia: Add more MainWindow styling

--- Bifrost.Avalonia/Views/MainWindow.axaml.cs
+++ Bifrost.Avalonia/Views/MainWindow.axaml.cs
@@ -1,5 +1,7 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
 using Bifrost.Core;
 using System;
 using System.IO;
@@ -8,22 +10,49 @@ namespace Bifrost.Avalonia.Views
 {
     public partial class MainWindow : Window
     {
+        private const string BackgroundOverrideFileName = "Bifrost.Background.png";
+
         private LaunchManager _launchManager;
 
         public MainWindow()
         {
             InitializeComponent();
+
+            ApplyBackgroundOverride();
+        }
+
+        private void ApplyBackgroundOverride()
+        {
+            if (File.Exists(BackgroundOverrideFileName) == false)
+                return;
+
+            try
+            {
+                Bitmap bitmap = new(BackgroundOverrideFileName);
+                ImageBrush imageBrush = new() { Source = bitmap };
+                MainGrid.Background = imageBrush;
+            }
+            catch (Exception)
+            {
+                return;
+            }
         }
 
         #region Event Handlers
 
         private void Window_Opened(object sender, EventArgs e)
         {
+            if (Design.IsDesignMode)
+                return;
+
             _launchManager = new();
 
             GameDirectoryInitializationResult result = _launchManager.GameDirectory.Initialize(Directory.GetCurrentDirectory());
             if (result != GameDirectoryInitializationResult.Success)
+            {
+                Close();
                 Environment.Exit(0);
+            }
 
             ServerComboBox.Items.Clear();
             foreach (Server server in _launchManager.ServerList)
@@ -37,6 +66,9 @@ namespace Bifrost.Avalonia.Views
 
         private void PlayButton_Click(object sender, RoutedEventArgs e)
         {
+            if (Design.IsDesignMode)
+                return;
+
             _launchManager.LaunchConfig.ServerIndex = ServerComboBox.SelectedIndex;
             _launchManager.SaveData();
             _launchManager.Launch();
